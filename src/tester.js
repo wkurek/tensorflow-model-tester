e4c6c0bac1e00402660cb87723869d58521ef6ec
wkurek <w.kurek2@gmail.com>
2019-06-15 12:10:22 +0200

    simple ejs rendering

--- src/tester.js
+++ src/tester.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
-const path = require("path");
+const { renderFile } = require("ejs");
+const { exec } = require("child_process");
 
 function test(graphPath, labelsPath, imagesDir, outputDir) {
-  fs.readdir(imagesDir, function(err, files) {
+  fs.readdir(imagesDir, (err, files) => {
     if (err) {
       throw err;
     }
 
-    files.forEach(function(filename) {
-      console.log(filename);
+    const options = { date: new Date(), images: [] };
+
+    files.forEach(filename => {
+      options.images.push({ path: filename });
+    });
+
+    renderFile("./views/report.ejs", options).then((err, str) => {
+      if (!err) console.log(str);
     });
   });
 }
